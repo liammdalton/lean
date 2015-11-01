3f0a40921f037c647439ead120219f820ea8fc81
Daniel Selsam <daniel.selsam@gmail.com>
2015-10-31 20:32:18 -0700

    More complete sketch of unelaborator

--- src/frontends/lean/unelaborator.cpp
+++ src/frontends/lean/unelaborator.cpp
@@ -7,24 +7,123 @@ Author: Leonardo de Moura
 #include "frontends/lean/unelaborator.h"
 #include "library/type_context.h"
 #include "library/app_builder.h"
+#include "kernel/abstract.h"
+#include "kernel/instantiate.h"
 
 namespace lean {
 
-expr unelaborate(environment const & env, io_state const & ios,
-                 list<expr> const & ctx, expr const & e) {
-    default_type_inference tc(env,ios,ctx);
-    app_builder builder(tc);
+class unelaborate_fn {
+    type_context & m_tc;
+    app_builder & m_builder;
+    list<expr> m_ctx;
 
+    expr unelaborate_binding(expr const & _e) {
+        expr d = unelaborate(binding_domain(e));
+        expr l = m_tc.mk_tmp_local(d, binding_info(e));
+        flet<list<expr>> add_to_ctx(m_ctx,cons(l,m_ctx));
+        expr b = abstract(unelaborate(instantiate(binding_body(e), l)), l);
+        return update_binding(e, d, b);
+    }
+    
+    expr unelaborate_app(expr const & e) {
+        buffer<expr> _args;
+        expr const & _f = get_app_args(e, args);
+
+        buffer<expr> args;
+        for (int i = 0; i < _args.size(); i++) {
+            args.push_back(unelaborate(_args[i]));
+        }
+
+        expr f = unelaborate(_f);
+
+        if (!is_constant(f)) return mk_app(f,args);
+
+        expr f_type = m_tc.infer(f);
+
+        buffer<bool> explicit_mask;
+        buffer<bool> higher_order_mask;
+
+        expr f_ = f;
+        while (is_pi(f_)) {
+            explicit_mask.push_back(is_explicit(binding_info(f_)));
+            higher_order_mask.push_back(is_pi(m_tc.whnf(binding_domain(f_))));
+            f_ = binding_body(f_);
+        }
+
+        expr actual = mk_app(f,args);
+        
+        /* (1) First we try only passing explicits. */
+        buffer<expr> explicit_args;
+        for (int i = 0; i < args.size(); i++) {
+            if (explicit_mask[i]) explicit_args.push_back(args[i]);
+        }
+
+        auto r_explicit = m_builder.mk_app(f,explicit_args.size(),explicit_args.data());
+
+        /* We are able to reconstruct the expression exactly while only looking 
+           at the explicit arguments. Thus we return the expression without any
+           additional annotation. */
+        if (r_explicit && *r_explicit = actual) return actual;
+        
+        /* (2) Next we try only passing explicits and higher-order implicits. */
+        buffer<expr> explicit_or_ho_args;
+        for (int i = 0; i < args.size(); i++) {
+            if (explicit_mask[i] || higher_order_mask[i])
+                explicit_or_ho_args.push_back(args[i]);
+        }
+
+        /* TODO this method does not exist yet. It may even be easier construct a new function with
+           modified binding_infos and call the regular [app_builder] makers. */
+        auto r_explicit_or_ho = m_builder.mk_app_partial(f,explicit_or_ho_args.size(),explicit_or_ho_args.data());
+
+        /* We are able to reconstruct the expression exactly while only looking 
+           at the explicit arguments. Thus we return the expression without any
+           additional annotation. */
+        if (r_explicit_or_ho && *r_explicit_or_ho = actual) return mk_partial_explicit(actual);
+
+        /* (3) We need to pass first-order implicit arguments as well. We can make this more precise
+           in the future by allowing [app_builder] to take a mask, and searching for implicit arguments
+           that we can print as underscores. */
+        return mk_explicit(actual);
+    }
+    
+    expr unelaborate(expr const & e) {
+        switch (e.kind()) {
+        case expr_kind::Meta:
+            throw exception("unelaborator only supports fully elaborated terms");
+            lean_unreachable();  // LCOV_EXCL_LINE
+        case expr_kind::Var:
+            lean_unreachable();  // LCOV_EXCL_LINE
+        case expr_kind::Macro:
+            // TODO we may need to support macros
+            throw exception("unelaborator does not support macros");
+        case expr_kind::Local: 
+        case expr_kind::Sort:
+        case expr_kind::Constant:
+            return e
+        case expr_kind::Lambda: 
+        case expr_kind::Pi:
+            return unelaborate_binding(e);
+        case expr_kind::App:
+            return unelaborate_app(e);
+        }
+        lean_unreachable();  // LCOV_EXCL_LINE    
+    }
+
+public:
+    unelaborator(type_context const & tc, app_builder const & builder, list<expr> const & ctx):
+        m_tc(tc), m_builder(builder), m_ctx(ctx) {}
+
+    expr operator()(expr const & e) { return unelaborate(e); }
+};
     
 
-    return e;
+expr unelaborate(environment const & env, io_state const & ios,
+                 list<expr> const & ctx, expr const & e) {
+    default_type_context tc(env,ios,ctx);
+    app_builder builder(tc);
+    return unelaborate_fn unelaborator(tc,builder,ctx)(e);
 }
-
-
-
-
-
-
 
 
 }
